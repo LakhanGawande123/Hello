acffa51b1bd34788c8d8f2c367debea7f832a917
LakhanGawande123 <lakhangawande@gmail.com>
2022-08-16 13:58:45 +0530

    New Changes Done Here

---
+++ HibernateSequenceExample/src/com/admin/Test.java
@@ -0,0 +1,26 @@
+package com.admin;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.model.Student;
+import com.util.HbUtil;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		SessionFactory sf = HbUtil.getSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		Student student = new Student();
+		student.setName("qwerty");
+		student.setAddress("Pune");
+		
+		session.save(student);
+		session.beginTransaction().commit();
+		
+	}
+
+}
